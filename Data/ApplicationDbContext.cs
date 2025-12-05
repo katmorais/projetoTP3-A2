4081c6362f0f029a916bf369c958b0ef0dd8cf03
Luahr Rocha <73291538+luahr13@users.noreply.github.com>
2025-12-05 00:52:01 -0300

    CRUD de Patologia com RESTRIÇÃO ao ADM

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -30,5 +30,6 @@ namespace projetoTP3_A2.Data
             });
         }
         public DbSet<projetoTP3_A2.Models.Medicamento> Medicamento { get; set; } = default!;
+        public DbSet<projetoTP3_A2.Models.Patologia> Patologia { get; set; } = default!;
     }
 }
